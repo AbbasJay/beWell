fc7c801ac40cc9a281f9eac3fe2c9be39b6b8277
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-16 17:37:16 +0000

    removed double item

--- app/ui/navigation-bar/navigation-bar.tsx
+++ app/ui/navigation-bar/navigation-bar.tsx
@@ -23,14 +23,12 @@ return (
     <CSS.Container>
 
         <CSS.LeftContainer>
-            <CSS.LeftItem>
                 {left?.map((item) => (
                     <CSS.LeftItem key={item.label} onPress={item.onPress}>
                         {item.icon}
                         {item.label && <CSS.LeftItemText>{item.label}</CSS.LeftItemText>}
-                    </CSS.LeftItem>
-                ))}
-            </CSS.LeftItem>
+                </CSS.LeftItem>
+            ))}
         </CSS.LeftContainer>
 
         <CSS.TitleContainer>
@@ -40,13 +38,13 @@ return (
 
         <CSS.RightContainer>
             {right?.map((item) => (
-                <CSS.RightItem key={item.label}  onPress={item.onPress}>
+                <CSS.RightItem key={item.label} onPress={item.onPress}>
                     {item.icon}
                     {item.label && <CSS.RightItemText>{item.label}</CSS.RightItemText>}
                 </CSS.RightItem>
             ))}
         </CSS.RightContainer>
-        
+
     </CSS.Container>
 )
 }
