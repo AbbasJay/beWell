9248136a86138e52b0696d320871669c22c126fa
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-16 16:43:13 +0000

    design tweaks, onPress and full width

--- app/ui/business-card/business-card.tsx
+++ app/ui/business-card/business-card.tsx
@@ -1,16 +1,33 @@
 import { Business } from "@/app/contexts/BusinessContext";
 import * as CSS from "./styles";
+import { View } from "react-native";
 
-export const BusinessCard = ({ item }: { item: Business }) => {
+type BusinessCardProps = {
+  item: Business;
+  onPress?: () => void;
+  fullWidth?: boolean;
+};
+
+export const BusinessCard = ({ item, onPress, fullWidth }: BusinessCardProps) => {
   return (
-    <CSS.Container>
-      <CSS.SampleImage/>
-      <CSS.ContentWrapper >
+    <View style={{ paddingHorizontal: 8 }}>
+      <CSS.Container onPress={onPress} fullWidth={fullWidth}>
+        <CSS.TopWrapper>
+        <CSS.CardHours>{item.hours}</CSS.CardHours>
+        <CSS.BookmarkIcon />
+      </CSS.TopWrapper>
+      <CSS.SampleImage 
+        source={require('@/assets/images/image 84.png')}
+        resizeMode="cover"
+        />
+      <CSS.ContentWrapper>
         <CSS.CardTitle>{item.name}</CSS.CardTitle>
-        <CSS.CardText>{item.address}</CSS.CardText>
-        <CSS.CardText>{item.description}</CSS.CardText>
-        <CSS.CardText>{item.phoneNumber}</CSS.CardText>
+        <CSS.CardAddress>{item.address}</CSS.CardAddress>
+        <CSS.CardDescription>{item.description}</CSS.CardDescription>
+        <CSS.CardPhoneNumber>{item.phoneNumber}</CSS.CardPhoneNumber>
+        <CSS.CardType>{item.type}</CSS.CardType>
       </CSS.ContentWrapper>
-    </CSS.Container>
+      </CSS.Container>
+    </View>
   );
 };
