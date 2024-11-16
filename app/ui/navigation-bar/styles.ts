b74ed5308ca70f243dbd5be9f90db0aaf3c641ef
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-16 17:37:40 +0000

    design tweaks

--- app/ui/navigation-bar/styles.ts
+++ app/ui/navigation-bar/styles.ts
@@ -5,11 +5,14 @@ width: 100%;
 display: flex;
 flex-direction: row;
 justify-content: space-between;
+align-items: center;
 `;
 
 export const TitleContainer = styled.View`
 display: flex;
 gap: 8px;
+flex: 2;
+align-items: center;
 `;
 
 export const Title = styled.Text`
@@ -26,6 +29,7 @@ export const LeftContainer = styled.View`
 display: flex;
 flex-direction: row;
 gap: 12px;
+flex: 1;
 `;
 
 export const LeftItem = styled.TouchableOpacity`
@@ -38,6 +42,8 @@ export const RightContainer = styled.View`
 display: flex;
 flex-direction: row;
 gap: 12px;
+flex: 1;
+justify-content: flex-end;
 `;
 
 export const RightItem = styled.TouchableOpacity`
