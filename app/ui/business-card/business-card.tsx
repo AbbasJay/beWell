40eac9bcbd4ecca9a55af9d0cfa3587c9cff16ea
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-16 15:37:33 +0000

    added sample image

--- app/ui/business-card/business-card.tsx
+++ app/ui/business-card/business-card.tsx
@@ -5,10 +5,12 @@ export const BusinessCard = ({ item }: { item: Business }) => {
   return (
     <CSS.Container>
       <CSS.SampleImage/>
+      <CSS.ContentWrapper >
         <CSS.CardTitle>{item.name}</CSS.CardTitle>
         <CSS.CardText>{item.address}</CSS.CardText>
         <CSS.CardText>{item.description}</CSS.CardText>
         <CSS.CardText>{item.phoneNumber}</CSS.CardText>
+      </CSS.ContentWrapper>
     </CSS.Container>
   );
 };
--- app/ui/business-card/styles.ts
+++ app/ui/business-card/styles.ts
@@ -4,9 +4,11 @@ import { styled } from "styled-components/native";
 export const Container = styled.View`
   background-color: #FFFFFF;
   padding: 10px 12px;
+  border-radius: 18px;
 `;
 
 export const Card = styled.View`
+
 `;
 
 export const CardTitle = styled.Text``;
@@ -14,8 +16,13 @@ export const CardTitle = styled.Text``;
 export const CardText = styled.Text``;
 
 export const SampleImage = styled.Image`
-  width: 200px;
   height: 140px;
   background-color: #000000;
-  border-radius: 10px;
+  border-radius: 18px;
+`;
+
+export const ContentWrapper = styled.View`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
