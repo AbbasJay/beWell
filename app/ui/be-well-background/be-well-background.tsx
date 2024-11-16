2983f2a6563f295da447c9f7cfd1f51d2c5165c9
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-16 12:41:32 +0000

    made be well background component dynamic

--- app/ui/be-well-background/be-well-background.tsx
+++ app/ui/be-well-background/be-well-background.tsx
@@ -1,9 +1,35 @@
 import styled from "styled-components/native";
-import { StatusBar, Platform } from "react-native";
+import { StatusBar, Platform, View } from "react-native";
 import { Colors } from "@/constants/Colors";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const BeWellBackground = styled.SafeAreaView`
+interface BeWellBackgroundProps {
+  children: React.ReactNode;
+  style?: any;
+  useSafeArea?: boolean;
+}
+
+const StyledView = styled.View`
   flex: 1;
-  background-color: ${Colors.dark.background};
+  background-color: ${Colors.light.primary};
   ${Platform.OS === 'android' ? `padding-top: ${StatusBar.currentHeight}px;` : ''}
 `;
+
+const StyledSafeAreaView = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${Colors.light.secondary};
+`;
+
+export const BeWellBackground: React.FC<BeWellBackgroundProps> = ({ 
+  children, 
+  style, 
+  useSafeArea = true 
+}) => {
+  const Container = useSafeArea ? StyledSafeAreaView : StyledView;
+  
+  return (
+    <Container style={style}>
+      {children}
+    </Container>
+  );
+};
