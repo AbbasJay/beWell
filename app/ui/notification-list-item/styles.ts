201b7df3251265dc140b7ff47742399ae35c0aaa
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-17 18:21:54 +0000

    progressing styles

---
+++ app/ui/notification-list-item/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+import { Colors } from "@/constants/Colors";
+
+export const Container = styled.View`
+  padding: 16px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${Colors.dark.secondary};
+`;
+
+export const SampleImage = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 10px;
+`;
+
+export const MessageText = styled.Text``;
+
+export const TimeText = styled.Text``;
