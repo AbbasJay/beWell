416aa5f61fce05c9f9e168d6b9ab426a0d8d75f2
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-18 14:55:54 +0000

    better adaptions

--- app/ui/be-well-background/be-well-background.tsx
+++ app/ui/be-well-background/be-well-background.tsx
@@ -1,51 +1,14 @@
-import React from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import * as CSS from "./styles";
+import { Colors } from "@/constants/Colors";
 
 interface BeWellBackgroundProps {
   children: React.ReactNode;
-  style?: any;
-  hasNavigationBar?: boolean;
-  fixedPaddingTop?: boolean;
-  scrollViewContentContainerStyle?: any;
-  useScrollView?: boolean;
+  scrollable?: boolean;
 }
 
-export const BeWellBackground: React.FC<BeWellBackgroundProps> = ({
+export const BeWellBackground = ({
   children,
-  style,
-  hasNavigationBar = false,
-  fixedPaddingTop = false,
-  scrollViewContentContainerStyle,
-  useScrollView = false,
-}) => {
-  const insets = useSafeAreaInsets();
-
-  const containerStyle = {
-    paddingTop: fixedPaddingTop ? insets.top : 0,
-    paddingBottom: hasNavigationBar ? insets.bottom : 0,
-    ...style,
-  };
-
-  return (
-    <CSS.StyledSafeAreaView style={containerStyle}>
-      {useScrollView ? (
-        <CSS.StyledScrollView
-          contentContainerStyle={{
-            ...scrollViewContentContainerStyle,
-          }}
-          showsVerticalScrollIndicator={false}
-        >
-          {children}
-        </CSS.StyledScrollView>
-      ) : (
-        <CSS.StyledView
-          hasNavigationBar={hasNavigationBar}
-          topInset={insets.top}
-        >
-          {children}
-        </CSS.StyledView>
-      )}
-    </CSS.StyledSafeAreaView>
-  );
+  scrollable = false,
+}: BeWellBackgroundProps) => {
+  return <CSS.StyledSafeAreaView>{children}</CSS.StyledSafeAreaView>;
 };
