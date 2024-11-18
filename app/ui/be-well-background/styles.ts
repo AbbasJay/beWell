b4647c1ef9417d4b30972a9ee62734f8906dfa86
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-18 13:51:41 +0000

    added styles.ts for bewellbackground

---
+++ app/ui/be-well-background/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, ScrollView } from "react-native";
+import { Colors } from "@/constants/Colors";
+
+export const StyledSafeAreaView = styled(SafeAreaView)<{
+  fixedPaddingTop?: boolean;
+}>`
+  flex: 1;
+  background-color: ${Colors.light.secondary};
+  padding-horizontal: 12px;
+  padding-top: ${({ fixedPaddingTop }) => (fixedPaddingTop ? "12px" : "0px")};
+`;
+
+export const StyledView = styled(View)<{
+  hasNavigationBar?: boolean;
+  topInset?: number;
+}>`
+  flex: 1;
+  padding-top: ${({ hasNavigationBar, topInset }) =>
+    hasNavigationBar ? `${topInset}px` : "0px"};
+`;
+
+export const StyledScrollView = styled(ScrollView)``;
