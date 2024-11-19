d72cb59587e66129ad45e87e13eeb09e1af0a398
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-19 13:33:54 +0000

    added skeleton, isLoading to false to be used

--- app/ui/business-card/business-card.tsx
+++ app/ui/business-card/business-card.tsx
@@ -1,5 +1,6 @@
 import { Business } from "@/app/contexts/BusinessContext";
 import * as CSS from "./styles";
+import { Skeleton } from "react-native-skeletons";
 
 type BusinessCardProps = {
   item: Business;
@@ -7,6 +8,7 @@ type BusinessCardProps = {
   disabled?: boolean;
   width?: string;
   height?: string;
+  isLoading?: boolean;
 };
 
 export const BusinessCard = ({
@@ -15,22 +17,34 @@ export const BusinessCard = ({
   width,
   height,
   disabled,
+  isLoading = false,
 }: BusinessCardProps) => {
   return (
     <CSS.Wrapper>
       <CSS.Container onPress={onPress} width={width} disabled={disabled}>
-        <CSS.ImageWrapper></CSS.ImageWrapper>
-        <CSS.SampleImage
-          height={height}
-          source={require("@/assets/images/home-gym.webp")}
-          resizeMode="cover"
-        />
-        <CSS.ContentWrapper>
-          <CSS.CardTitle>{item.name}</CSS.CardTitle>
-          <CSS.Info>{item.address}</CSS.Info>
-          <CSS.Info>{item.phoneNumber}</CSS.Info>
-          <CSS.Info>{item.type}</CSS.Info>
-        </CSS.ContentWrapper>
+        {isLoading ? (
+          <>
+            <Skeleton height={80} width="100%" />
+            <Skeleton height={20} width="60%" style={{ marginTop: 10 }} />
+            <Skeleton height={20} width="80%" style={{ marginTop: 5 }} />
+            <Skeleton height={20} width="40%" style={{ marginTop: 5 }} />
+          </>
+        ) : (
+          <>
+            <CSS.ImageWrapper></CSS.ImageWrapper>
+            <CSS.SampleImage
+              height={height}
+              source={require("@/assets/images/home-gym.webp")}
+              resizeMode="cover"
+            />
+            <CSS.ContentWrapper>
+              <CSS.CardTitle>{item.name}</CSS.CardTitle>
+              <CSS.Info>{item.address}</CSS.Info>
+              <CSS.Info>{item.phoneNumber}</CSS.Info>
+              <CSS.Info>{item.type}</CSS.Info>
+            </CSS.ContentWrapper>
+          </>
+        )}
       </CSS.Container>
     </CSS.Wrapper>
   );
