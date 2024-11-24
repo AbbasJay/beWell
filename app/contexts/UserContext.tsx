b6c86a9e624b7af845a728012e8a47c2e831f39f
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-24 21:34:51 +0000

    created user context

---
+++ app/contexts/UserContext.tsx
@@ -0,0 +1,78 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+import { API_URL } from "@/env";
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+type UserContextType = {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+};
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        let token;
+        if (Platform.OS === "web") {
+          token = localStorage.getItem("userToken");
+        } else {
+          token = await SecureStore.getItemAsync("userToken");
+        }
+
+        if (!token) {
+          throw new Error("No authentication token found");
+        }
+
+        const response = await fetch(`${API_URL}/api/user`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          const errorText = await response.text();
+          console.error("Error response:", errorText);
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const userData = await response.json();
+        setUser(userData);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUserContext must be used within a UserProvider");
+  }
+  return context;
+};
