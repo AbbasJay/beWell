1c20167dbcb0a54a18f0ef09db6e162ab75f1a56
hussymuss <hussain_jabbar1@hotmail.com>
2024-11-25 09:50:39 +0000

    changed to default async

--- app/utils/helper-functions/get-token.ts
+++ app/utils/helper-functions/get-token.ts
@@ -1,8 +1,8 @@
 import { Platform } from "react-native";
 import * as SecureStore from "expo-secure-store";
 
-export const getToken = async (): Promise<string | null> => {
+export default async function getToken(): Promise<string | null> {
   return Platform.OS === "web"
     ? localStorage.getItem("userToken")
     : await SecureStore.getItemAsync("userToken");
-};
+}
