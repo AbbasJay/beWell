4071f1a00027aceb49f00c3fbbc59a684524dc1f
Hussomuss <hussain_jabbar1@hotmail.com>
2025-07-09 20:48:44 +0100

    Added data.ts to prep review

---
+++ app/utils/components-data/class-reviews-data.ts
@@ -0,0 +1,12 @@
+export type Review = {
+  id: string;
+  userId: string;
+  userName: string;
+  userAvatar: string;
+  rating: number;
+  comment: string;
+  createdAt: string;
+  updatedAt: string;
+  likedCount: number;
+  dislikedCount: number;
+};
