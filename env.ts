f51168695abc0a13ac3d5b5ba501c7ce21565027
Hussomuss <hussain_jabbar1@hotmail.com>
2025-07-26 12:08:02 +0100

    env change?

--- env.ts
+++ env.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://192.168.0.52:3000";
-// export const API_URL = "https://be-well-web.vercel.app";
+// export const API_URL = "http://192.168.0.52:3000";
+export const API_URL = "https://be-well-web.vercel.app";
